a9391d157715bd5d7be1f3293a564eba9ca5c7a6
Lorenzo Panieri <panierilorenzo@gmail.com>
2023-08-27 15:09:11 +0200

    add: drgns error handler

---
+++ drgns/src/error_handler.rs
@@ -0,0 +1,109 @@
+//! Error reporting module, keeps track of all errors during compilation.
+//!
+//! Note that lints as not supported yet, these are all hard-coded errors,
+//! whereas lints can be disabled or enabled by the user. Perhaps in the
+//! future we will have lint reporting.
+//!
+//! ## Code Ranges
+//! Each error has a code, but equivalent errors of different severities,
+//! e.g. `err_io_generic` and `warn_io_generic` share the same code.
+//!
+//! - range `00xxx`: generic or top-level errors, used also for CLI errors
+//!     - `00000`: unused, represents lack of errors
+//!     - `00001`: generic error
+//! - range `01xxx`: I/O errors
+//! - range `02xxx`: syntax errors
+//!
+//! ## Error Severities
+//! - warn:  the program will compile, but will likey fail at run-time or if
+//!          requirements change
+//! - err:   the program cannot be compiled, this is a normal part of
+//!          development workflow, so we need to provide high-quality
+//!          reporting
+//! - fatal: the compiler did something it shouldn't have done. These errors
+//!          are the bad ones, they should never occur.
+
+use std::sync::{
+    atomic::{AtomicBool, Ordering},
+    OnceLock, RwLock, RwLockReadGuard, RwLockWriteGuard,
+};
+
+/// Print the errors collected so far and returns the most appropriate UNIX
+/// error code.
+pub fn display_errors() -> i32 {
+    let er = error_reader();
+    for e in er.iter() {
+        println!("{}", e);
+    }
+    // TODO: right now it always returns 0 or 1
+    if HAS_ERROR.load(Ordering::Relaxed) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+/// Reports an uncategorized error, should be considered as a To-Do for adding
+/// new error types in later versions.
+pub fn err_generic(msg: impl std::fmt::Debug) {
+    const CODE: u32 = 00001;
+    push_error(CODE, msg);
+}
+
+pub fn err_io_not_found(path: &str) {
+    const CODE: u32 = 01001;
+    push_error(CODE, "path ".to_owned() + path + " does not exist");
+}
+
+/// Same as `err_generic` but fatal (doesn't attempt to recover).
+pub fn fatal_generic(msg: &str) -> ! {
+    const CODE: u32 = 00001;
+    crash_and_burn(CODE, msg);
+}
+
+pub fn fatal_unreachable() -> ! {
+    const CODE: u32 = 00002;
+    crash_and_burn(CODE, "unreachable code was reached");
+}
+
+// TODO: store errors as a data type, rather than raw String
+static ERRORS: OnceLock<RwLock<Vec<String>>> = OnceLock::new();
+static HAS_ERROR: AtomicBool = AtomicBool::new(false);
+
+/// Get singleton writer to underlying error vector
+fn error_writer() -> RwLockWriteGuard<'static, Vec<String>> {
+    ERRORS
+        .get_or_init(|| RwLock::new(vec![]))
+        .write()
+        .unwrap_or_else(|_| fatal_generic("poisoned lock"))
+}
+
+/// Get singleton reader to underlying error vector, prefer using this over
+/// `error_writer` as it is more efficient
+fn error_reader() -> RwLockReadGuard<'static, Vec<String>> {
+    ERRORS
+        .get_or_init(|| RwLock::new(vec![]))
+        .read()
+        .unwrap_or_else(|_| fatal_generic("poisoned lock"))
+}
+
+fn push_error(code: u32, msg: impl std::fmt::Debug) {
+    let mut eh = error_writer();
+    HAS_ERROR.store(true, Ordering::Relaxed);
+    eh.push(format!("[E{:0>5}] {:#?}", code, msg));
+}
+
+fn crash_and_burn(code: u32, msg: impl std::fmt::Debug) -> ! {
+    const FATAL_COPYPASTA: &str = concat!(
+        "The compiler encountered an internal fatal error, from which it",
+        "cannot safely recover. This is most likely a bug in the compiler, ",
+        "please reach out to the developer by reporting an issue on github:\n",
+        "https://github.com/PanieriLorenzo/dragon-script/issues\n",
+        "include the following in the issue title: "
+    );
+
+    HAS_ERROR.store(true, Ordering::Relaxed);
+
+    println!("[F{:0>5}] {} {:#?}", code, FATAL_COPYPASTA, msg);
+    panic!("{:#?}", msg);
+}
--- drgns/src/main.rs
+++ drgns/src/main.rs
@@ -1,10 +1,17 @@
-use clap::Parser;
+use clap::{Error, Parser};
+use error_handler::{display_errors, err_io_not_found, fatal_unreachable};
+use std::{
+    process::{exit, ExitCode},
+    sync::{OnceLock, OnceState, RwLock},
+};
+
+mod error_handler;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    #[arg(short, long, required = false)]
-    input: String,
+    #[arg(short, long)]
+    input: Option<String>,
 
     #[arg(short, long, action, default_value_t = false, required = false)]
     check: bool,
@@ -12,4 +19,27 @@ struct Args {
 
 fn main() {
     let args = Args::parse();
+    if let Some(path) = args.input.as_deref() {
+        // run in batch mode
+        let _ = run_file(path);
+    } else {
+        // run in REPL mode
+        run_prompt();
+    }
+    std::process::exit(display_errors());
 }
+
+fn run_file(path: &str) {
+    let source = match std::fs::read_to_string(path) {
+        Ok(src) => src,
+        Err(err) => match err.kind() {
+            std::io::ErrorKind::NotFound => {
+                err_io_not_found(path);
+                return;
+            }
+            _ => fatal_unreachable(),
+        },
+    };
+}
+
+fn run_prompt() {}
